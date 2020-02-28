ae008518167f248f6d88e488fa8c9f6065d21061
Revati Tekale <revitekale1910@gmail.com>
2020-02-28 14:28:46 +0530

    ADDED:Flipped coin

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,3 +1,13 @@
-#!/bin/bash -x 
-echo "Flip Coin Simulation Program"
+#!/bin/bash -x
+
+echo "Welcome to Flip Coin Simulation"
+
+random=$((RANDOM%2))
+
+if [[ $random -eq 1 ]]
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
 
