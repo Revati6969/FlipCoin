a6e03e4f75b7aa07430e299999cf3e4c97661e3b
Revati Tekale <revitekale1910@gmail.com>
2020-02-28 14:33:09 +0530

    ADDED:Singlet Combination and its Percentage

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,13 +1,29 @@
 #!/bin/bash -x
 
-echo "Welcome to Flip Coin Simulation"
+declare -A Dictionary
 
-random=$((RANDOM%2))
+echo "Enter the times to toss the coin"
+read times
+Head=0
+Tail=0
 
-if [[ $random -eq 1 ]]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+for (( index=0; index<$times; index++ ))
+do
+	random=$((RANDOM%2))
+	if [[ $random -eq 1 ]]
+	then
+		Head=$(($Head+1))
+	else
+		Tail=$(($Tail+1))
+	fi
+done
 
+Dictionary[Head]=$Head
+Dictionary[Tail]=$Tail
+
+echo ${Dictionary[@]}
+echo ${!Dictionary[@]}
+head=`expr "scale=3;( ${Dictionary[Head]} / $times ) * 100" | bc -l`
+echo $head
+tail=`expr "scale=3;( ${Dictionary[Tail]} / $times ) * 100" | bc -l`
+echo $tail
