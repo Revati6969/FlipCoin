76ffd60d2dd505f7126d77797eccd20bba81d5a7
Revati Tekale <revitekale1910@gmail.com>
2020-02-28 14:56:03 +0530

    ADDED:Sorted and displayed winner

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -3,43 +3,47 @@
 
 declare -A Dictionary
 
+#Variables
+max=0
+
 echo "Enter the times to toss the coin"
 read times
 echo "Enter number of coins 1)Single 2)Double 3)Triple: "
-
 read coins
 
 function toss()
 {
-
 for (( index=0; index<$times; index++ ))
 do
-	str=""
-	for (( index1=0; index1<$coins; index1++ ))
-	do	
-		random=$((RANDOM%2))
-		if [[ $random -eq 1 ]]
-		then
-			str=$str"H"
-		else
-			str=$str"T"
-		fi
-	done
-
+   str=""
+   for (( index1=0; index1<$coins; index1++ ))
+   do	
+      random=$((RANDOM%2))
+      if [[ $random -eq 1 ]]
+      then
+          str=$str"H"
+      else
+          str=$str"T"
+      fi
+   done
 Dictionary[$str]=$(( ${Dictionary[$str]} + 1 ))
 done
-
-for keys in ${!Dictionary[@]}
-do
-	percentage=`expr "scale=2;( ${Dictionary[$keys]} / $times ) * 100" | bc -l`
-	echo $keys = $percentage
-
-done
-
 }
-
 toss
 
-echo ${!Dictionary[@]}
-echo ${Dictionary[@]}
+echo "Dictionary in descending order"
 
+for k in "${!Dictionary[@]}"
+do
+   echo $k ' - ' ${Dictionary["$k"]}
+done | sort -rn -k3
+
+for k in "${!Dictionary[@]}"
+do
+   if [[ $max -lt ${Dictionary["$k"]} ]]
+   then
+    max=${Dictionary["$k"]}
+    key1=$k
+   fi
+done
+echo "Winning Combination : $key1 -->  $max"
